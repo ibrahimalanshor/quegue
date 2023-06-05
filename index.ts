dc8264ce5324af4595a527126c0b4580c70553d6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-05 19:53:03 +0700

    feat: setup server

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { createServer } from './lib/server/server';
+
+const server = createServer();
+
+server.listen();
---
+++ lib/server/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+
+interface ServerConfig {
+  port: number;
+}
+
+class Server {
+  express: express.Application;
+  config: ServerConfig = {
+    port: 3000,
+  };
+
+  constructor(express: express.Application, config?: Partial<ServerConfig>) {
+    this.express = express;
+
+    if (config) {
+      this.config.port = config.port || 3000;
+    }
+  }
+
+  listen(cb?: (port: number) => void) {
+    const port = this.config.port || 3000;
+
+    this.express.listen(port, () => {
+      cb ? cb(port) : console.log(`server running at ${port}`);
+    });
+  }
+}
+
+export function createServer(): Server {
+  return new Server(express());
+}
