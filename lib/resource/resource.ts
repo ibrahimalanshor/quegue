5a87630f061859e71c062e29cf3fd11f51df67d4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-08 07:27:43 +0700

    feat: resource model

--- lib/resource/resource.ts
+++ lib/resource/resource.ts
@@ -1,21 +1,31 @@
 import { knex } from '../knex/knex';
 import { Stored } from '../entity/types';
-import { FindOptions, Resource, ResourceModel } from './resource.type';
+import { FindOptions, Resource } from './resource.type';
 import { createWhereBuilder } from './resource.helper';
 
-class ResourceService<T, C> {
+export abstract class ResourceModel {
+  abstract table: string;
+  abstract fillable: string[];
+  abstract selectable: string[];
+}
+
+export class ResourceService<T, C> {
   constructor(public model: ResourceModel) {}
 
   async findOne(options: FindOptions): Promise<Stored<T>> {
     const res = await knex(this.model.table)
       .where(createWhereBuilder(options.filter))
-      .first(this.model.fillable);
+      .first(this.model.selectable);
 
     return res as Stored<T>;
   }
 
   async store(values: C): Promise<Stored<T>> {
-    const [storedId] = await knex(this.model.table).insert(values);
+    const fillableValues = Object.fromEntries(
+      this.model.fillable.map((col: string) => [col, values[col as keyof C]])
+    );
+
+    const [storedId] = await knex(this.model.table).insert(fillableValues);
 
     return this.findOne({
       filter: {
--- lib/resource/resource.type.ts
+++ lib/resource/resource.type.ts
@@ -1,18 +1,8 @@
-import { Stored } from '../entity/types';
+import { ResourceService } from './resource';
 
 export interface Resource<T, C> {
   service: ResourceService<T, C>;
 }
-export interface ResourceModel {
-  table: string;
-  fillable: string[];
-  selectable: string[];
-}
-export interface ResourceService<T, C> {
-  findOne(options: FindOptions): Promise<Stored<T>>;
-  store(values: C): Promise<T>;
-}
-
 export interface FilterOptions {
   operator?: string;
   value: any;
