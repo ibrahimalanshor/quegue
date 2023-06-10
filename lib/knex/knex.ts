bf40e67972b0f690b9332661f5113c3b8ae17391
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-10 08:31:36 +0700

    feat: db config

--- lib/knex/knex.ts
+++ lib/knex/knex.ts
@@ -1,5 +1,12 @@
 import Knex from 'knex';
+import { dbConfig } from '../../src/config/db.config';
 
 export const knex = Knex({
   client: 'mysql2',
+  connection: {
+    user: dbConfig.user,
+    host: dbConfig.host,
+    password: dbConfig.password,
+    database: dbConfig.name,
+  },
 });
