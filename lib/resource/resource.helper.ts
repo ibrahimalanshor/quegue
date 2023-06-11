d7bd018ddf3d4881317142b499bd5be9246d5a39
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-06-11 19:33:27 +0700

    feat: resource filter is null

--- lib/resource/resource.helper.ts
+++ lib/resource/resource.helper.ts
@@ -7,11 +7,15 @@ export function createWhereBuilder(
 ): (builder: Knex.QueryBuilder) => void {
   return (builder: Knex.QueryBuilder) => {
     for (const key in filters) {
-      builder.where(
-        knex.raw(key),
-        filters[key].operator ?? '=',
-        filters[key].value
-      );
+      if (filters[key].operator === 'null') {
+        builder.whereNull(key);
+      } else {
+        builder.where(
+          knex.raw(key),
+          filters[key].operator ?? '=',
+          filters[key].value
+        );
+      }
     }
   };
 }
